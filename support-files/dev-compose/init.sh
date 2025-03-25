41543e9fe679b3b473bf98d7293db67db81e4fb0
小埋酱 <81210463+etherfurnace@users.noreply.github.com>
2025-03-25 15:46:08 +0800

    remove folder

--- support-files/dev-compose/init.sh
+++
@@ -1,7 +0,0 @@
-nats -s nats://127.0.0.1:4222 \
-    --user admin --password password \
-        stream add metrics --subjects="metrics.*" --storage=file \
-        --replicas=1 --retention=limits  --discard=old \
-        --max-age=20m --max-bytes=104857600 --max-consumers=-1 \
-        --max-msg-size=-1 --max-msgs=-1 --max-msgs-per-subject=1000000 \
-        --dupe-window=5m --no-allow-rollup --no-deny-delete --no-deny-purge 
