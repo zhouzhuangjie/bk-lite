c86279312607023be87f472d5039c0294afbd4c6
baiyufei <baiyfyx@163.com>
2025-04-11 14:16:24 +0800

    feature-补充jetstream与executor能力

--- server/apps/rpc/executor.py
+++ server/apps/rpc/executor.py
@@ -1,4 +1,4 @@
-from server.apps.rpc.base import RpcClient
+from apps.rpc.base import RpcClient
 
 
 class Executor(object):
@@ -10,21 +10,21 @@ class Executor(object):
         self.instance_id = instance_id
         self.local_client = RpcClient('local.execute')
         self.ssh_client = RpcClient('ssh.execute')
+        self.download_to_local_client = RpcClient('download.local')
+        self.download_to_remote_client = RpcClient('download.remote')
 
-    def execute_local(self, command, working_directory=None, timeout=60):
+    def execute_local(self, command, timeout=60):
         """
         执行本地命令
         :param command: 要执行的命令
-        :param working_directory: 工作目录
         :param timeout: 执行超时时间(秒)
         :return: 命令执行结果
         """
         request_data = {
             "command": command,
-            "working_directory": working_directory,
-            "timeout": timeout
+            "execute_timeout": timeout
         }
-        return_data = self.local_client.run(self.instance_id, **request_data)
+        return_data = self.local_client.run(self.instance_id, request_data)
         return return_data
 
     def execute_ssh(self, command, host, username, password=None, key_file=None, timeout=60):
@@ -41,8 +41,8 @@ class Executor(object):
         request_data = {
             "command": command,
             "host": host,
-            "username": username,
-            "timeout": timeout
+            "user": username,
+            "execute_timeout": timeout
         }
 
         # 添加可选参数
@@ -51,5 +51,53 @@ class Executor(object):
         if key_file:
             request_data["key_file"] = key_file
 
-        return_data = self.ssh_client.run(self.instance_id, **request_data)
+        return_data = self.ssh_client.run(self.instance_id, request_data)
+        return return_data
+
+    def download_to_local(self, bucket_name, file_key, file_name, target_path, timeout=60):
+        """
+        下载文件
+        :param bucket_name: 存储桶名称
+        :param file_key: 文件在存储桶中的键
+        :param file_name: 文件名称
+        :param target_path: 本地目标路径
+        :param timeout: 执行超时时间(秒)
+        :return: 下载结果
+        """
+        request_data = {
+            "bucket_name": bucket_name,
+            "file_key": file_key,
+            "file_name": file_name,
+            "target_path": target_path,
+            "execute_timeout": timeout
+        }
+        return_data = self.download_to_local_client.run(self.instance_id, request_data)
+        return return_data
+
+    def download_to_remote(self, bucket_name, file_key, file_name, target_path, host, username, password=None, timeout=60):
+        """
+        下载文件到远程
+        :param bucket_name: 存储桶名称
+        :param file_key: 文件在存储桶中的键
+        :param file_name: 文件名称
+        :param target_path: 远程目标路径
+        :param host: 远程主机地址
+        :param username: SSH用户名
+        :param password: SSH密码(可选)
+        :param timeout: 执行超时时间(秒)
+        :return: 下载结果
+        """
+        request_data = {
+            "bucket_name": bucket_name,
+            "file_key": file_key,
+            "file_name": file_name,
+            "target_path": target_path,
+            "host": host,
+            "user": username,
+            "execute_timeout": timeout,
+        }
+        # 添加可选参数
+        if password:
+            request_data["password"] = password
+        return_data = self.download_to_remote_client.run(self.instance_id, request_data)
         return return_data
