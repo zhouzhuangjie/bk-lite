3a82d7846d401deac2f8cc019e592b1961cb8614
小埋酱 <81210463+etherfurnace@users.noreply.github.com>
2025-04-14 19:52:13 +0800

    build: ``` Add and configure monitor service with Docker support ```
    - Add a new Dockerfile for the monitor service, including Python 3.10, essential dependencies, and setup scripts.
    - Include commented commands for monitor and CMDB initialization in the startup script.
    
    Signed-off-by: 小埋酱 <81210463+etherfurnace@users.noreply.github.com>

--- server/support-files/docker/startup.sh
+++ server/support-files/docker/startup.sh
@@ -6,4 +6,12 @@ python manage.py collectstatic --noinput
 #python manage.py init_realm
 #python manage.py init_realm_resource
 
+# node
+# python manage.py node_init
+
+# monitor
+# python manage.py plugin_init
+
+# cmdb
+# python manage.py model_init
 supervisord -n
