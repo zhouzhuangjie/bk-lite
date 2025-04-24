900629014893c8c1c23193b059aee337e5a95802
小埋酱 <81210463+etherfurnace@users.noreply.github.com>
2025-04-24 16:47:33 +0800

    feat: enhance model loading with local files restriction
    - Add `local_files_only=True` parameter to `TextEmbedding` initialization to restrict model loading to local files only.
    
    Signed-off-by: 小埋酱 <81210463+etherfurnace@users.noreply.github.com>

--- metis/src/embed/embed_builder.py
+++ metis/src/embed/embed_builder.py
@@ -7,6 +7,6 @@ class EmbedBuilder:
         model_type = protocol.split(':')[1]
         model_name = protocol.split(':')[2]
         if model_type == 'text_embedding':
-            return TextEmbedding(model_name=model_name, cache_dir="models")
+            return TextEmbedding(model_name=model_name, cache_dir="models",local_files_only=True)
         else:
             raise ValueError(f"Unsupported model type: {model_type}")
