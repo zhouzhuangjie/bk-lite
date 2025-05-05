e4ad3473d9231b5a0a4d7a0faa06f5d0348d8250
小埋酱 <81210463+etherfurnace@users.noreply.github.com>
2025-05-05 11:19:47 +0800

    feat: enhance document chunking and PDF processing
    - Add logic in `FixedSizeChunk`, `RecursiveChunk`, and `SemanticChunk` classes to group documents by `segment_number` and assign a `chunk_number` to each document within the group.
    - Ensure `FullChunk` assigns a default `chunk_number` of `0` to all documents.
    - Update `PDFLoader` to enhance PDF processing by extracting text while skipping table areas, parsing images with improved metadata, and refining table parsing with page number support.
    - Refactor `PDFLoader` to support a mode-based document segmentation (`full` for entire document or `page` for per-page documents).
    - Add a new `docker-compose.dev.yml` file to define services for PostgreSQL and Elasticsearch, including configurations for volumes, ports, and basic environment settings.
    
    Signed-off-by: 小埋酱 <81210463+etherfurnace@users.noreply.github.com>

--- metis/src/chunk/recursive_chunk.py
+++ metis/src/chunk/recursive_chunk.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import List
 
 from langchain_core.documents import Document
@@ -15,4 +16,18 @@ class RecursiveChunk:
 
     def chunk(self, docs: List[Document]) -> List[Document]:
         split_docs = self.text_splitter.split_documents(docs)
+
+        # 2. 按照 segment_number 分组
+        grouped_docs = defaultdict(list)
+        for doc in split_docs:
+            segment_number = doc.metadata.get('segment_number', 0)
+            grouped_docs[segment_number].append(doc)
+
+        # 3. 为每个组内的文档添加 chunk_number
+        result_docs = []
+        for segment_number, segment_docs in grouped_docs.items():
+            for chunk_number, doc in enumerate(segment_docs):
+                doc.metadata['chunk_number'] = chunk_number
+                result_docs.append(doc)
+
         return split_docs
--- metis/src/chunk/semantic_chunk.py
+++ metis/src/chunk/semantic_chunk.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import List
 
 from langchain_core.documents import Document
@@ -12,5 +13,18 @@ class SemanticChunk:
                                                 sentence_split_regex=r'(?<=[.?!。？！])\s*')
 
     def chunk(self, docs: List[Document]) -> List[Document]:
-        docs = self.semantic_chunker.split_documents(docs)
-        return docs
+        split_docs = self.semantic_chunker.split_documents(docs)
+
+        # 2. 按照 segment_number 分组
+        grouped_docs = defaultdict(list)
+        for doc in split_docs:
+            segment_number = doc.metadata.get('segment_number', 0)
+            grouped_docs[segment_number].append(doc)
+
+        # 3. 为每个组内的文档添加 chunk_number
+        result_docs = []
+        for segment_number, segment_docs in grouped_docs.items():
+            for chunk_number, doc in enumerate(segment_docs):
+                doc.metadata['chunk_number'] = chunk_number
+                result_docs.append(doc)
+        return split_docs
