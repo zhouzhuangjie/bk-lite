00c1242c10b0bd924d025e5c2627065e062445e5
小埋酱 <81210463+etherfurnace@users.noreply.github.com>
2025-05-05 11:50:47 +0800

    feat: add TextRank algorithm for NLP keyword and sentence extraction
    - Add jieba library to requirements for text processing.
    - Introduce an empty `__init__.py` in the `algorithm` and `textrank` directories.
    - Add stopwords file containing commonly filtered words for Chinese and English language processing.
    - Implement a `WordSegmentation` class with support for stop words filtering and speech tags filtering.
    - Add `SentenceSegmentation` and `Segmentation` classes to facilitate sentence and word segmentation workflows.
    - Create a `TextRankKeyword` class for extracting keywords and key phrases using the TextRank algorithm.
    - Add a `TextRankSentence` class for extracting key sentences for text summarization.
    - Implement utility functions to manage text formatting, word combinations, sentence similarity, and TextRank graph creation.
    - Create unit tests for TextRank keyword and sentence extraction using a sample input (`Readme.md`).
    
    Signed-off-by: 小埋酱 <81210463+etherfurnace@users.noreply.github.com>

---
+++ metis/tests/algorithm/textrank_test.py
@@ -0,0 +1,25 @@
+from src.algorithm.textrank.textrank_keyword import TextRankKeyword
+from src.algorithm.textrank.textrank_sentence import TextRankSentence
+
+
+def test_textrank():
+    with open('../Readme.md', 'r', encoding='utf-8') as f:
+        text = f.read()
+
+        trkw = TextRankKeyword()
+        trkw.analyze(text=text, lower=True, window=2)
+
+        print('关键词：')
+        for item in trkw.get_keywords(3, word_min_len=1):
+            print(item.word, item.weight)
+
+        print('关键短语：')
+        for phrase in trkw.get_keyphrases(keywords_num=3, min_occur_num=2):
+            print(phrase)
+
+        trs = TextRankSentence()
+        trs.analyze(text=text, lower=True, source='all_filters')
+
+        print('摘要：')
+        for item in trs.get_key_sentences(num=3):
+            print(item.index, item.weight, item.sentence)
