ead16f0d36ae916717279efb98c843ace9d50974
justin <justinzhou@canway.net>
2025-05-14 16:12:02 +0800

    feature:metis add ansible,http_request tools

--- metis/src/tools/http_request_tools.py
+++ metis/src/tools/http_request_tools.py
@@ -1,21 +1,142 @@
 from typing import Dict
 
+import json_repair
 from langchain_core.runnables import RunnableConfig
 from langchain_core.tools import tool
+from loguru import logger
+
+import requests
 
 
-@tool(parse_docstring=True)
-def http_get_tool(param: str,config: RunnableConfig):
+def update_api_config(dynamics: Dict, config: RunnableConfig) -> Dict:
     """
-
-    Args:
-        param:
-
-    Returns:
-
+    更新API配置,用params进行参数替换
     """
     url = config['configurable']['url']
+    headers = config['configurable'].get("headers") or '{}'
+    api_params = config['configurable'].get("params") or '{}'
+    data = config['configurable'].get("data") or '{}'
+    # 替换url中的参数
+    for key, value in dynamics.items():
+        url = url.replace(f'{{{{{key}}}}}', str(value))
+        headers = headers.replace(f'{{{{{key}}}}}', str(value))
+        api_params = api_params.replace(f'{{{{{key}}}}}', str(value))
+        data = data.replace(f'{{{{{key}}}}}', str(value))
+    try:
+        headers = json_repair.loads(headers)
+        data = json_repair.loads(data)
+    except:
+        raise ValueError("headers or data is not a valid JSON string")
+    return {
+        'url': url,
+        'headers': headers,
+        'params': api_params,
+        'data': data
+    }
 
-# post
-# delete
-# put
+
+@tool()
+def http_get(dynamics: str, config: RunnableConfig):
+    """
+    这是一个HTTP GET请求的工具，用户可以通过这个工具向指定的URL发送GET请求，并获取响应数据。
+
+    Args:
+        dynamics (str): 包含请求的动态参数，例如 {"id": 123}。
+        config (RunnableConfig): 包含请求的配置参数。
+            - configurable (dict): 包含以下字段：
+                - url (str): 请求的URL，如果有路径参数需要使用{{}}进行替换，例如 http://example.com/{{id}}。
+                - headers (str): 请求头，如果有参数需要使用{{}}进行替换，例如 {"contentType":"Application/json"}。
+                - params (str): 请求参数，如果有参数需要使用{{}}进行替换，例如 {"uuid": "{{uuid}}" }。
+                - data (str): 请求体，如果有参数需要使用{{}}进行替换，例如 {"name": "{{name}}" }。
+
+    Returns:
+        str: 响应数据。
+    """
+    dynamics = json_repair.loads(dynamics)
+    api_kwargs = update_api_config(dynamics, config)
+    api_kwargs.update(data=None)
+    return _request("GET", **api_kwargs, )
+
+
+@tool()
+def http_post(dynamics: str, config: RunnableConfig):
+    """
+    这是一个HTTP POST请求的工具，用户可以通过这个工具向指定的URL发送POST请求，并获取响应数据。
+
+    Args:
+        dynamics (str): 包含请求的动态参数，例如 {"id": 123}。
+        config (RunnableConfig): 包含请求的配置参数。
+            - configurable (dict): 包含以下字段：
+                - url (str): 请求的URL，如果有路径参数需要使用{{}}进行替换，例如 http://example.com/{{id}}。
+                - headers (str): 请求头，如果有参数需要使用{{}}进行替换，例如 {"contentType":"Application/json"}。
+                - params (str): 请求参数，如果有参数需要使用{{}}进行替换，例如 {"uuid": "{{uuid}}" }。
+                - data (str): 请求体，如果有参数需要使用{{}}进行替换，例如 {"name": "{{name}}" }。
+
+    Returns:
+        str: 响应数据。
+    """
+    dynamics = json_repair.loads(dynamics)
+    api_kwargs = update_api_config(dynamics, config)
+    return _request("POST", **api_kwargs)
+
+
+@tool()
+def http_put(dynamics: str, config: RunnableConfig):
+    """
+    这是一个HTTP PUT请求的工具，用户可以通过这个工具向指定的URL发送PUT请求，并获取响应数据。
+
+    Args:
+        dynamics (str): 包含请求的动态参数，例如 {"id": 123}。
+        config (RunnableConfig): 包含请求的配置参数。
+            - configurable (dict): 包含以下字段：
+                - url (str): 请求的URL，如果有路径参数需要使用{{}}进行替换，例如 http://example.com/{{id}}。
+                - headers (str): 请求头，如果有参数需要使用{{}}进行替换，例如 {"contentType":"Application/json"}。
+                - params (str): 请求参数，如果有参数需要使用{{}}进行替换，例如 {"uuid": "{{uuid}}" }。
+                - data (str): 请求体，如果有参数需要使用{{}}进行替换，例如 {"name": "{{name}}" }。
+
+    Returns:
+        str: 响应数据。
+    """
+    dynamics = json_repair.loads(dynamics)
+    api_kwargs = update_api_config(dynamics, config)
+    return _request("PUT", **api_kwargs)
+
+
+@tool()
+def http_delete(dynamics: str, config: RunnableConfig):
+    """
+    这是一个HTTP DELETE请求的工具，用户可以通过这个工具向指定的URL发送DELETE请求，并获取响应数据。
+
+    Args:
+        dynamics (str): 包含请求的动态参数，例如 {"id": 123}。
+        config (RunnableConfig): 包含请求的配置参数。
+            - configurable (dict): 包含以下字段：
+                - url (str): 请求的URL，如果有路径参数需要使用{{}}进行替换，例如 http://example.com/{{id}}。
+                - headers (str): 请求头，如果有参数需要使用{{}}进行替换，例如 {"contentType":"Application/json"}。
+                - params (str): 请求参数，如果有参数需要使用{{}}进行替换，例如 {"uuid": "{{uuid}}" }。
+                - data (str): 请求体，如果有参数需要使用{{}}进行替换，例如 {"name": "{{name}}" }。
+
+    Returns:
+        str: 响应数据。
+    """
+    dynamics = json_repair.loads(dynamics)
+    api_kwargs = update_api_config(dynamics, config)
+    return _request("DELETE", **api_kwargs)
+
+
+def _request(method: str, url: str, headers: Dict, params: Dict, data: Dict):
+    """
+    发送HTTP请求
+    :param method: 请求方法
+    :param url: 请求URL
+    :param headers: 请求头
+    :param params: URL参数
+    :param data: 请求体数据
+    :return:
+    """
+    try:
+        response = requests.request(method, url, headers=headers, params=params, json=data, verify=False)
+        logger.info(f"请求URL: {url}, 请求方法: {method}, 请求头: {headers}, 请求参数: {params}, 请求体: {data}")
+        return response.content.decode('utf-8')
+    except requests.exceptions.RequestException as e:
+        raise ValueError(f"请求失败: {e}")
--- metis/tests/agent/react_agent_test.py
+++ metis/tests/agent/react_agent_test.py
@@ -67,3 +67,87 @@ async def test_react_agent_with_jenkins_tools():
         graph = ReActAgentGraph()
         result = await graph.execute(request)
         logger.info(result)
+
+
+@pytest.mark.asyncio
+async def test_react_agent_with_ansible_tools():
+    tools_servers: List[ToolsServer] = [
+        ToolsServer(name="ansible_adhoc", url='langchain:ansible', extra_prompt={}),
+    ]
+
+    request = ReActAgentRequest(
+        model="gpt-4o",
+        openai_api_base=os.getenv("OPENAI_BASE_URL"),
+        openai_api_key=os.getenv("OPENAI_API_KEY"),
+        user_message="mysql有几张库",
+        user_id="umr",
+        thread_id="2",
+        tools_servers=tools_servers,
+        extra_config={
+            "module": "mysql_info",
+            "module_args": "login_user=root login_host=127.0.0.1 login_password=123456",
+        }
+    )
+    graph = ReActAgentGraph()
+
+    logger.info(f"messages 模式")
+    result = await graph.stream(request)
+    await graph.aprint_chunk(result)
+    print('\n')
+
+
+@pytest.mark.asyncio
+async def test_react_agent_with_ansible_inventory_tools():
+    tools_servers: List[ToolsServer] = [
+        ToolsServer(name="ansible_adhoc", url='langchain:ansible'),
+    ]
+
+    request = ReActAgentRequest(
+        model="gpt-4o",
+        openai_api_base=os.getenv("OPENAI_BASE_URL"),
+        openai_api_key=os.getenv("OPENAI_API_KEY"),
+        user_message="这些机器的主机名叫啥",
+        user_id="umr",
+        thread_id="2",
+        tools_servers=tools_servers,
+        extra_config={
+            "inventory": os.getenv("TEST_ANSIBLE_INVENTORY"),
+            "module": "command",
+            "module_args": "hostname",
+        }
+    )
+    graph = ReActAgentGraph()
+
+    logger.info(f"messages 模式")
+    result = await graph.stream(request)
+    await graph.aprint_chunk(result)
+    print('\n')
+
+
+@pytest.mark.asyncio
+async def test_react_agent_with_http_request_tools():
+    tools_servers: List[ToolsServer] = [
+        ToolsServer(name="http_request", url='langchain:http_request', extra_prompt={
+            "limit": "这个可以限制取多少条数据",
+        }),
+    ]
+
+    request = ReActAgentRequest(
+        model="gpt-4o",
+        openai_api_base=os.getenv("OPENAI_BASE_URL"),
+        openai_api_key=os.getenv("OPENAI_API_KEY"),
+        user_message="热门电影前十展示",
+        user_id="umr",
+        thread_id="2",
+        tools_servers=tools_servers,
+        extra_config={
+            "url": "https://m.douban.com/rexxar/api/v2/subject/recent_hot/movie?limit={{limit}}",
+            "headers": '{"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36","referer":"https://movie.douban.com/","Origin":"https://movie.douban.com"}',
+        }
+    )
+    graph = ReActAgentGraph()
+
+    logger.info(f"messages 模式")
+    result = await graph.stream(request)
+    await graph.aprint_chunk(result)
+    print('\n')
